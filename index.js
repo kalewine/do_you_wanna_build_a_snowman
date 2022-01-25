2eeb07cf3f42bcf5c521565ae29689d291bb7837
kalewine <kayla.r.alewine@gmail.com>
2022-01-25 09:26:24 -0500

    Fix background gradient display and add transformations to snowballs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// toggle screen
 const toggle = document.querySelector('input');
 const body = document.querySelector("body");
 
@@ -11,3 +12,19 @@ toggle.addEventListener('change', e => {
 })
 
 
+const base = document.getElementById('base');
+const middle = document.getElementById('middle');
+const top = document.getElementById('top');
+
+const baseActivator = document.getElementById('activate-1');
+const middleActivator = document.getElementById('activate-2');
+const topActivator = document.getElementById('activate-3');
+
+
+baseOptions= {};
+
+const baseObserver = new IntersectionObserver((entries, baseActivator) => {
+    base.classList.add('activate')
+}, baseOptions )
+
+baseObserver.observer(baseActivator)
